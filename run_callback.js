2eb910a9463a088a24ecb4032bfc77a11a179812
Omkardilwale <omkar.dilwale.ovd@gmail.com>
2022-03-09 12:37:15 +0530

    6

--- run_callback.js
+++ run_callback.js
@@ -1,7 +1,6 @@
 function RunCallback(a, b, cb) {
-    // Write you code here, you need to add a and b, pass the result into callback function cb as argument return the result
-    let Result1 =a+b;
-    return cb(Result1);
+    let Result =a+b;
+    return cb(Result);
 }
 
 module.exports = RunCallback;
